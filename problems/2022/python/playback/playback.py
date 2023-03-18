232580b490837f58a6b63ae99cb5dcc636f7d852
nazrul007 <nazrul.islam@gmail.com>
2023-03-18 12:57:15 -0700

    Playback Speed problem: https://cs50.harvard.edu/python/2022/psets/0/playback/

---
+++ problems/2022/python/playback/playback.py
@@ -0,0 +1,21 @@
+# Problem Definition 
+# Some people have a habit of lecturing speaking rather quickly, and it’d be nice to slow them down, 
+# a la YouTube’s 0.75 playback speed, or even by having them pause between words.
+#
+# In a file called playback.py, implement a program in Python that prompts the user for input and then 
+# outputs that same input, replacing each space with ... (i.e., three periods).
+# 
+# Hints
+# Recall that input returns a str, per docs.python.org/3/library/functions.html#input.
+# Recall that a str comes with quite a few methods, per docs.python.org/3/library/stdtypes.html#string-methods.
+
+def replace(what: str) -> str: 
+     
+    return what.replace(' ', '...')
+
+def main(): 
+    userInput = str( input("What would you like me to repeat? ") )
+    print (replace(userInput))
+
+if (__name__ == "__main__"):
+    main() 
