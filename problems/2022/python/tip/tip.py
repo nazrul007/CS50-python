80f0cbe73f5634911cbc0e0d1daeb07c5ff11b75
nazrul007 <nazrul.islam@gmail.com>
2023-03-18 14:48:40 -0700

    Minor update

--- problems/2022/python/tip/tip.py
+++ problems/2022/python/tip/tip.py
@@ -1,13 +1,16 @@
 def main():
+    """
+    main driver function provided by assignment. It asks for total meal amount, tip percent and 
+    then calculates the actial tip. 
+    """    
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-
-
 def dollars_to_float(d) -> float:
-    """dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a 
+    """
+    dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a 
     decimal digit), remove the leading $, and return the amount as a float. For instance, given $50.00 as 
     input, it should return 50.0.
 
@@ -24,10 +27,10 @@ def dollars_to_float(d) -> float:
     return float(dollar)
 
 def percent_to_float(p) -> float:
-    """percent_to_float, which should accept a str as input (formatted as ##%, wherein each # is a decimal digit), 
+    """
+    percent_to_float, which should accept a str as input (formatted as ##%, wherein each # is a decimal digit), 
     remove the trailing %, and return the percentage as a float. For instance, given 15% as input, it should 
     return 0.15.
-
 
     Args:
         p (str): percentage. For example, 15%.
